1af924b0aaae0bc1d7b1a3cf4706c4fc24923d47
Lalli <laruskonrad@hotmail.com>
2021-01-28 12:48:14 +0000

    Added function that generates a random message and displays it in console.log

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const startOfSentenceArray = ['Good morning ', 'Good evening ', 'Hello there ', 'Greetings '];
+const middleOfSentenceArray = ['darling ', 'my friend ', 'you there '];
+const endOfSentenceArray = ['have a good day!', 'have a lovely evening!', 'see you later, friend!', 'I hope to see you again!'];
+
+const createMessage = (startArray, middleArray, endArray) => {
+    const start = startArray[Math.floor(Math.random() * startArray.length)];
+    const middle = middleArray[Math.floor(Math.random() * middleArray.length)];
+    const end = endArray[Math.floor(Math.random() * endArray.length)]
+
+    return start + middle + end;
+}
+
+const randonMessage = createMessage(startOfSentenceArray, middleOfSentenceArray, endOfSentenceArray);
+
+console.log(randomMessage);
